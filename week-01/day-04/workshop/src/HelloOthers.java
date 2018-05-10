da65951f20c66b5651a683153e3b0a83dfae0f35
ronai22 <ronai22@gmail.com>
2018-05-10 12:53:36 +0200

    New workshop folder, printing to the terminal window exercises 01,02,03

---
+++ week-01/day-04/workshop/src/HelloOthers.java
@@ -0,0 +1,16 @@
+
+public class HelloOthers {
+    public static void main(String[] args) {
+        // Greet 3 of your classmates with this program, in three separate lines
+        // like:
+        //
+        // Hello, Esther!
+        // Hello, Mary!
+        // Hello, Joe!
+
+        System.out.println("Hello, Dani!");
+        System.out.println("Hello, Attila!");
+        System.out.println("Hello, Franci!");
+
+    }
+}
