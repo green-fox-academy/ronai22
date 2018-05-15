e3e9fd2ec92e29b3b9e36d050298fd5a377210c1
ronai22 <ronai22@gmail.com>
2018-05-15 11:32:06 +0200

    Modification in Ex19

--- week-02/day-01/workshop/src/Ex19Reverse.java
+++ week-02/day-01/workshop/src/Ex19Reverse.java
@@ -1,3 +1,5 @@
+import java.util.Arrays;
+
 public class Ex19Reverse {
   public static void main(String[] args) {
     // - Create an array variable named `aj`
@@ -5,12 +7,12 @@ public class Ex19Reverse {
     // - Reverse the order of the elements in `aj`
     // - Print the elements of the reversed `aj`
     int[] aj = {3, 4, 5, 6, 7};
-    int[] temp = {3, 4, 5, 6, 7};
+    // int[] temp = {3, 4, 5, 6, 7};
+    int[] temp = aj;
+
 
     for (int i = 0; i < aj.length ; i++) {
       aj[i] = temp[(aj.length - 1) - i];
-      System.out.println(aj[i]);
-    }
-
+    } System.out.println(Arrays.toString(aj));
   }
 }
