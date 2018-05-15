b86c2d9fe0601af46ca20d9ad5f254c1665bbd61
ronai22 <ronai22@gmail.com>
2018-05-15 12:37:04 +0200

    StringsAndArraylists Exercises 01, 02 done

---
+++ week-02/day-02/workshop/src/Ex01SimpleReplace.java
@@ -0,0 +1,11 @@
+public class Ex01SimpleReplace {
+  public static void main(String... args){
+    String example = "In a dishwasher far far away";
+
+    // I would like to replace "dishwasher" with "galaxy" in this example, but it has a problem.
+    // Please fix it for me!
+    // Expected ouput: In a galaxy far far away
+
+    System.out.println(example.replace("dishwasher", "galaxy"));
+  }
+}
---
+++ week-02/day-02/workshop/src/Ex02UrlFixer.java
@@ -0,0 +1,23 @@
+public class Ex02UrlFixer {
+  public static void main(String[] args) {
+    String url = "https//www.reddit.com/r/nevertellmethebots";
+
+    // Accidentally I got the wrong URL for a funny subreddit. It's probably "odds" and not "bots"
+    // Also, the URL is missing a crutial component, find out what it is and insert it too!
+    // Try to solve it more than once using different String functions!
+
+    // url = url.replace("https", "https:" );
+    // url = url.replace("bots", "odds");
+
+    // url = url.substring(0, 5) + ":" + url.substring(5, url.length());
+    // int index = url.indexOf("bots", 4);
+    // url = url.substring(0, index) + "odds";
+
+    String[] parts = url.split("/" , 2);
+    url = parts[0] + ":/" + parts[1];
+    String[] parts2 = url.split("bots", 2);
+    url = parts2[0] + "odds";
+
+    System.out.println(url);
+  }
+}
