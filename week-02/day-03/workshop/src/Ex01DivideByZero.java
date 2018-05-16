6e38341150f3b870abf0dbbed1fdfb7f0aa107dc
ronai22 <ronai22@gmail.com>
2018-05-16 11:59:51 +0200

    week-02 day-03 workshop ex01 done

---
+++ week-02/day-03/workshop/src/Ex01DivideByZero.java
@@ -0,0 +1,20 @@
+import java.util.Scanner;
+
+public class Ex01DivideByZero {
+  public static void main(String[] args) {
+    // Create a function that takes a number
+    // divides ten with it,
+    // and prints the result.
+    // It should print "fail" if the parameter is 0
+    Scanner scanner = new Scanner(System.in);
+    System.out.println("Please type in a number, I am going to divide 10 with it.");
+    int divisor = scanner.nextInt();
+
+    try {
+      int result = 10 / divisor;
+      System.out.println(result);
+    } catch (ArithmeticException e) {
+      System.out.println("Can't divide by zero!");
+    }
+  }
+}
