8a115dc18b68872201077eaf6fe5c67e5ae2f4c1
ronai22 <ronai22@gmail.com>
2018-05-18 11:35:03 +0200

    Ex-02 halfway

---
+++ week-02/day-05/workshop/src/Ex02LinePlayQuarter.java
@@ -0,0 +1,57 @@
+import javax.swing.*;
+
+import java.awt.*;
+import java.util.Random;
+
+import static javax.swing.JFrame.EXIT_ON_CLOSE;
+
+public class Ex02LinePlayQuarter {
+  public static void mainDraw(Graphics graphics) {
+    int stepsSize = 10;
+    int canvasDivide = 2;
+    patternDrawer(stepsSize, canvasDivide, graphics);
+  }
+
+  private static void patternDrawer(int stepSize, int canvasDivide, Graphics graphics) {
+    for (int canvasX = 0; canvasX < canvasDivide ; canvasX++) {
+      for (int canvasY = 0; canvasY < canvasDivide ; canvasY++) {
+        for (int i = 1; i < WIDTH / stepSize / canvasDivide ; i++) {
+          lineDrawer(i, canvasX, canvasY, stepSize, canvasDivide, graphics);
+        }
+      }
+    }
+  }
+
+  private static void lineDrawer(int i, int canvasX, int canvasY, int stepSize, int canvasDivide, Graphics graphics) {
+
+    graphics.setColor(Color.RED);
+    graphics.drawLine(
+            canvasX * HEIGHT / canvasDivide + i * stepSize,
+            canvasY * HEIGHT / canvasDivide,
+            HEIGHT / canvasDivide +  canvasX * (HEIGHT / canvasDivide),
+            canvasY * HEIGHT / canvasDivide + i * stepSize
+    );
+    
+  }
+
+  // Don't touch the code below
+  static int WIDTH = 400;
+  static int HEIGHT = 400;
+
+  public static void main(String[] args) {
+    JFrame jFrame = new JFrame("Ex02LinePlayQuarter");
+    jFrame.setSize(new Dimension(WIDTH, HEIGHT + 23));
+    jFrame.setDefaultCloseOperation(EXIT_ON_CLOSE);
+    jFrame.add(new ImagePanel());
+    jFrame.setLocationRelativeTo(null);
+    jFrame.setVisible(true);
+  }
+
+  static class ImagePanel extends JPanel {
+    @Override
+    protected void paintComponent(Graphics graphics) {
+      super.paintComponent(graphics);
+      mainDraw(graphics);
+    }
+  }
+}
