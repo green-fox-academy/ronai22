7de86442e8de34e5cf0a333f6d3c7b0f3a7e2615
ronai22 <ronai22@gmail.com>
2018-05-18 10:30:34 +0200

    Ex01 done with random colours

--- week-02/day-05/workshop/src/Ex01LinePlay.java
+++ week-02/day-05/workshop/src/Ex01LinePlay.java
@@ -1,22 +1,28 @@
 import javax.swing.*;
 
 import java.awt.*;
+import java.util.Random;
 
 import static javax.swing.JFrame.EXIT_ON_CLOSE;
 
 public class Ex01LinePlay {
   public static void mainDraw(Graphics graphics) {
-    int steps = 20;
-    patternDrawer(steps, graphics);
+    int stepsSize = 20;
+    patternDrawer(stepsSize, graphics);
   }
 
-  private static void patternDrawer(int steps, Graphics graphics) {
-    for (int i = 0; i < WIDTH / steps ; i++) {
-      graphics.setColor(Color.GREEN);
-      graphics.drawLine(0 + i * steps, 0, WIDTH, 0 + i * steps);
-      graphics.setColor(Color.MAGENTA);
-      graphics.drawLine(0, 0 + i * steps, 0 + i * steps, WIDTH);
+  private static void patternDrawer(int stepSize, Graphics graphics) {
+    for (int i = 0; i < WIDTH / stepSize ; i++) {
+      lineDrawer(i, stepSize, graphics);
     }
+  }
+
+  private static void lineDrawer(int i, int stepSize, Graphics graphics) {
+    Random random = new Random();
+    graphics.setColor(new Color(random.nextInt(256), random.nextInt(256), random.nextInt(256)));
+    graphics.drawLine(i * stepSize, 0, HEIGHT, i * stepSize);
+    graphics.setColor(new Color(random.nextInt(256), random.nextInt(256), random.nextInt(256)));
+    graphics.drawLine(0,i * stepSize, i * stepSize, WIDTH);
   }
 
   // Don't touch the code below
