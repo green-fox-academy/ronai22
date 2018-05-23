788d824ac2cf7c879d661e3f14760b98e9436199
ronai22 <ronai22@gmail.com>
2018-05-23 12:52:08 +0200

    Ex-02 BlogPost done

---
+++ week-03/day-02/BlogPost/src/BlogPost.java
@@ -0,0 +1,23 @@
+import java.util.Date;
+
+public class BlogPost {
+
+  String authorName;
+  String title;
+  String text;
+  String date;
+
+  @Override
+  public String toString() {
+    return "\"" + this.title + "\" " + "titled by " + this.authorName + " posted at \"" + this.date + "\" \n " +
+            this.text;
+  }
+
+  public BlogPost(String authorName, String title, String text, String date) {
+    this.authorName = authorName;
+    this.title = title;
+    this.text = text;
+    this.date = date;
+
+  }
+}
