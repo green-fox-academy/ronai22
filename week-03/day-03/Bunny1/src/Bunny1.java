43f13d456c57f89d07b15abe16a57864a36d3478
ronai22 <ronai22@gmail.com>
2018-05-24 12:10:13 +0200

    Ex-05 done, two versions

---
+++ week-03/day-03/Bunny1/src/Bunny1.java
@@ -0,0 +1,15 @@
+public class Bunny1 {
+  public static void main(String[] args) {
+
+    System.out.println(bunnyEarCounter(100));
+  }
+
+  private static int bunnyEarCounter(int bunnyNumber) {
+    int bunnyEars = 2;
+    if (bunnyNumber == 1) {
+      return bunnyEars;
+    } else {
+      return bunnyEars + bunnyEarCounter(bunnyNumber - 1);
+    }
+  }
+}
---
+++ week-03/day-03/Bunny1/src/Bunny2.java
@@ -0,0 +1,17 @@
+public class Bunny2 {
+  public static void main(String[] args) {
+
+    int bunnyNumber = 100;
+    int bunnyEars = 0;
+
+    System.out.println(bunnyEarCounter(bunnyNumber, bunnyEars));
+  }
+
+  private static int bunnyEarCounter(int bunnyNumber, int bunnyEars) {
+    if (bunnyNumber == 0) {
+      return bunnyEars;
+    } else {
+      return bunnyEarCounter(bunnyNumber - 1, bunnyEars + 2);
+    }
+  }
+}
