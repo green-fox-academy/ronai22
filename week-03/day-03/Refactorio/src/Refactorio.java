0d4dcb1629582d91f14c90efcc30d8ae8e85a666
ronai22 <ronai22@gmail.com>
2018-05-24 17:09:42 +0200

    Ex-11 done

---
+++ week-03/day-03/Refactorio/src/Refactorio.java
@@ -0,0 +1,14 @@
+public class Refactorio {
+  public static void main(String[] args) {
+    int n = 6;
+    System.out.println(refactorio(n));
+  }
+
+  private static int refactorio(int n) {
+    if (n == 0){
+      return 1;
+    } else {
+      return n * refactorio(n - 1);
+    }
+  }
+}
