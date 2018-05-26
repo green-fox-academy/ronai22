eae264eae050f0b099b62ce3371f92b2d49dde0c
ronai22 <ronai22@gmail.com>
2018-05-26 13:41:30 +0200

    Pirate exercise Pirates part done

---
+++ week-03/day-02/Pirates/src/Pirate.java
@@ -0,0 +1,52 @@
+import java.util.Random;
+
+public class Pirate {
+
+  static int idCounter = 1;
+
+  int pirateId;
+  int drunkIndex;
+  int healthIndex;
+
+  public Pirate() {
+    pirateId = idCounter++;
+    drunkIndex = 0;
+    healthIndex = 2;
+  }
+
+  public void drinkSomeRum(){
+    if (healthIndex == 0) {
+      System.out.println("He is dead.");
+    } else {
+      drunkIndex = drunkIndex++;
+    }
+  }
+
+  public void howsItGoingMate(){
+    if (drunkIndex <=4)
+      System.out.println("Pour me anudder");
+    else  {
+      System.out.println("Arghh, I'ma Pirate. How d'ya d'ink its goin?");
+      healthIndex--;
+    }
+  }
+
+  public void die() {
+    healthIndex = 0;
+  }
+
+  public void brawl (Pirate pirate){
+    if (pirate.healthIndex == 2) {
+      Random random = new Random();
+      int fightResult = random.nextInt(3);
+      if (fightResult == 0) {
+        pirate.healthIndex = 0;
+        } else if (fightResult == 1) {
+        this.healthIndex = 0;
+      } else {
+        pirate.healthIndex = 1;
+        this.healthIndex =1;
+      }
+    }
+  }
+}
