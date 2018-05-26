d036fe83d7c7dd43395ae280ed9b38be640b4f5e
ronai22 <ronai22@gmail.com>
2018-05-26 11:26:39 +0200

    String 3 exercise corrected to fit more the styleguide (Thank you Norbi)

--- week-03/day-03/String3/src/String3.java
+++ week-03/day-03/String3/src/String3.java
@@ -2,16 +2,15 @@ public class String3 {
   public static void main(String[] args) {
 
     String sentence = "Given a string, compute recursively a new string, where all the adjacent chars are now separated by a *";
-    System.out.println(xChanger(sentence));
+    System.out.println(starSeparatorAdder(sentence));
   }
 
-  private static String xChanger(String sentence) {
+  private static String starSeparatorAdder(String sentence) {
     char first;
     if (sentence.length() < 1) {
       return sentence;
-    } else {
-      first = sentence.charAt(0);
-      return first + "*" + xChanger(sentence.substring(1));
-    }
+    } 
+    first = sentence.charAt(0);
+    return first + "*" + starSeparatorAdder(sentence.substring(1));
   }
 }
