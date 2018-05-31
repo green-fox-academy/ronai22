99067a134d4dd04d30059d23eba0796d4cd10ada
ronai22 <ronai22@gmail.com>
2018-05-31 13:53:53 +0200

    removed main part to the test

--- week-04/day-04/Workshop/src/Anagram/Anagram.java
+++ week-04/day-04/Workshop/src/Anagram/Anagram.java
@@ -4,15 +4,6 @@ import java.util.Arrays;
 
 public class Anagram {
 
-  public static void main(String[] args) {
-
-    String input1 = "i am a God";
-    String input2 = "idog Maa";
-
-    boolean anagramResult = isAnagram(input1, input2);
-    System.out.println(anagramResult);
-  }
-
   private static boolean isAnagram(String input1, String input2) {
 
     if (orderedInput(input1).equals(orderedInput(input2))) {
@@ -23,11 +14,9 @@ public class Anagram {
 
   private static String orderedInput(String input) {
     String orderedInputString;
-
     char[] charArray = spaceRemover(input).toLowerCase().toCharArray();
     Arrays.sort(charArray);
     orderedInputString = new String(charArray);
-
     return orderedInputString;
   }
 
