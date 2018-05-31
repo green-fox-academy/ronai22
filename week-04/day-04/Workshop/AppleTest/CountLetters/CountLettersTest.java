8a246478ce5bded2f592a8b2c3b4a2c4e8222d48
ronai22 <ronai22@gmail.com>
2018-05-31 16:37:54 +0200

    run 6 tests, passed every of them

--- week-04/day-04/Workshop/AppleTest/CountLetters/CountLettersTest.java
+++ week-04/day-04/Workshop/AppleTest/CountLetters/CountLettersTest.java
@@ -1,11 +1,8 @@
 package CountLetters;
-
 import org.junit.BeforeClass;
 import org.junit.Test;
-
 import java.util.HashMap;
 import java.util.Map;
-
 import static org.junit.Assert.*;
 
 public class CountLettersTest {
@@ -29,7 +26,6 @@ public class CountLettersTest {
   @Test
   public void should_giveBack_sameSize() {
     Map<String, Integer> letterMap = new HashMap<>();
-
     String input = "abcdefa";
     letterMap.put("a",2);
     letterMap.put("b",1);
@@ -37,8 +33,45 @@ public class CountLettersTest {
     letterMap.put("d",1);
     letterMap.put("e",1);
     letterMap.put("f",1);
-
     assertEquals(letterMap.size(), countLetters.letterCounter(input).size());
   }
 
+  @Test
+  public void should_beTheSame_withSpecialCharacters() {
+    Map<String, Integer> letterMap = new HashMap<>();
+    String input = ":--!:+";
+    letterMap.put(":",2);
+    letterMap.put("-",2);
+    letterMap.put("!",1);
+    letterMap.put("+",1);
+    assertEquals(letterMap, countLetters.letterCounter(input));
+  }
+
+  @Test
+  public void should_beTheSame_withEmptyHashmap() {
+    Map<String, Integer> letterMap = new HashMap<>();
+    String input = "";
+    assertEquals(letterMap, countLetters.letterCounter(input));
+  }
+
+  @Test
+  public void should_beTheSame_withSpacesInTheText() {
+    Map<String, Integer> letterMap = new HashMap<>();
+    String input = "ab ab ab";
+    letterMap.put("a",3);
+    letterMap.put("b",3);
+    letterMap.put(" ",2);
+    assertEquals(letterMap, countLetters.letterCounter(input));
+  }
+
+  @Test
+  public void should_beTheSame_withCapitalLetters() {
+    Map<String, Integer> letterMap = new HashMap<>();
+    String input = "ab Ab ab";
+    letterMap.put("a",2);
+    letterMap.put("b",3);
+    letterMap.put(" ",2);
+    letterMap.put("A",1);
+    assertEquals(letterMap, countLetters.letterCounter(input));
+  }
 }
