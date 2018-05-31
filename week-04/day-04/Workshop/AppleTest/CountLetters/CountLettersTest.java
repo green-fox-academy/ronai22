f232e7383a62bca47cae7f5f500052e84670445d
ronai22 <ronai22@gmail.com>
2018-05-31 16:31:03 +0200

    runs 2 test

--- week-04/day-04/Workshop/AppleTest/CountLetters/CountLettersTest.java
+++ week-04/day-04/Workshop/AppleTest/CountLetters/CountLettersTest.java
@@ -1,12 +1,44 @@
 package CountLetters;
 
+import org.junit.BeforeClass;
 import org.junit.Test;
+
+import java.util.HashMap;
+import java.util.Map;
 
 import static org.junit.Assert.*;
 
 public class CountLettersTest {
+  static CountLetters countLetters;
+
+  @BeforeClass
+  public static void init() {
+    countLetters = new CountLetters();
+  }
 
   @Test
-  public void countLetters() {
+  public void should_giveBack_sameHashMap() {
+    Map<String, Integer> letterMap = new HashMap<>();
+    String input = "abca";
+    letterMap.put("a",2);
+    letterMap.put("b",1);
+    letterMap.put("c",1);
+    assertEquals(letterMap, countLetters.letterCounter(input));
   }
+
+  @Test
+  public void should_giveBack_sameSize() {
+    Map<String, Integer> letterMap = new HashMap<>();
+
+    String input = "abcdefa";
+    letterMap.put("a",2);
+    letterMap.put("b",1);
+    letterMap.put("c",1);
+    letterMap.put("d",1);
+    letterMap.put("e",1);
+    letterMap.put("f",1);
+
+    assertEquals(letterMap.size(), countLetters.letterCounter(input).size());
+  }
+
 }
--- week-04/day-04/Workshop/src/CountLetters/CountLetters.java
+++ week-04/day-04/Workshop/src/CountLetters/CountLetters.java
@@ -5,15 +5,9 @@ import java.util.Map;
 
 public class CountLetters {
 
-  static String input;
-
-  public CountLetters() {
-  }
-
-  public static Map<String, Integer> countLetters(String input) {
-    Map<String, Integer> letterMap = new HashMap<String, Integer>();
-
-    for (int i = 0; i < input.length() ; i++) {
+  public Map<String, Integer> letterCounter(String input) {
+    Map<String, Integer> letterMap = new HashMap<>();
+    for (int i = 0; i < input.length(); i++) {
       if (letterMap.containsKey(input.valueOf(input.charAt(i)))) {
         int count = letterMap.containsKey(input.valueOf(input.charAt(i))) ? letterMap.get(input.valueOf(input.charAt(i))) : 0;
         letterMap.put(input.valueOf(input.charAt(i)), count + 1);
