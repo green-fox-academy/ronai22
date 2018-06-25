b2ab0c042c3fcf44a883b227242009ef58211a36
ronai22 <ronai22@gmail.com>
2018-06-25 13:06:20 +0200

    (init)springbootstart: initialize project

---
+++ week-07/day-01/Springstart/src/main/java/com/greenfoxacademy/springstart/SpringstartApplication.java
@@ -0,0 +1,15 @@
+package com.greenfoxacademy.springstart;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.stereotype.Controller;
+
+@Controller
+@SpringBootApplication
+public class SpringstartApplication {
+  public static void main(String[] args) {
+    SpringApplication.run(SpringstartApplication.class, args);
+  }
+
+  
+}
