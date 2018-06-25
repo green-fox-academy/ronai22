fcb9ddbb26907c36856f901695e29357dae1f01d
ronai22 <ronai22@gmail.com>
2018-06-25 15:08:22 +0200

    last exercise finished

--- week-07/day-01/Springstart/src/main/java/com/greenfoxacademy/springstart/controllers/HelloWebController.java
+++ week-07/day-01/Springstart/src/main/java/com/greenfoxacademy/springstart/controllers/HelloWebController.java
@@ -2,18 +2,24 @@ package com.greenfoxacademy.springstart.controllers;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RequestParam;
+import java.util.Random;
 
-import java.util.concurrent.atomic.AtomicLong;
 
 @Controller
 public class HelloWebController {
-  AtomicLong atomiclong = new AtomicLong();
+  Random random = new Random();
+  String[] hellos = {"Mirëdita", "Ahalan", "Parev", "Zdravei", "Nei Ho", "Dobrý den", "Ahoj", "Goddag", "Goede dag, Hallo", "Hello", "Saluton", "Hei", "Bonjour",
+          "Guten Tag", "Gia'sou", "Aloha", "Shalom", "Namaste", "Namaste", "Jó napot", "Halló", "Helló", "Góðan daginn", "Halo", "Aksunai", "Qanuipit", "Dia dhuit",
+          "Salve", "Ciao", "Kon-nichiwa", "An-nyong Ha-se-yo", "Salvëte", "Ni hao", "Dzien' dobry", "Olá", "Bunã ziua", "Zdravstvuyte", "Hola", "Jambo", "Hujambo", "Hej",
+          "Sa-wat-dee", "Merhaba", "Selam", "Vitayu", "Xin chào", "Hylo", "Sut Mae", "Sholem Aleychem", "Sawubona"};
+
+  String[] colors = { "Tomato", "Orange", "DodgerBlue", "MediumSeaGreen", "Gray", "SlateBlue", "Violet", "LightGray"};
 
   @RequestMapping("/web/greeting")
-  public String greeting(Model model, @RequestParam(value = "name") String name) {
-    model.addAttribute("name", " " + name);
-    model.addAttribute("id", atomiclong.incrementAndGet());
+  public String greeting(Model model) {
+    model.addAttribute("name", hellos[random.nextInt(hellos.length - 1)]);
+    model.addAttribute("color", colors[random.nextInt(colors.length - 1)]);
+    model.addAttribute("fontSize", 12 + random.nextInt(40));
     return "greeting";
   }
 }
