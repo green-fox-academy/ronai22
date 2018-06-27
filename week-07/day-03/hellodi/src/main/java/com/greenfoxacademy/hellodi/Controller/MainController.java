a78795d9dbcecdab47358ffe530cb1d7f4ef4765
ronai22 <ronai22@gmail.com>
2018-06-27 15:26:02 +0200

    feat(exercise-day3): add ceasars encoding/decoding

--- week-07/day-03/hellodi/src/main/java/com/greenfoxacademy/hellodi/Controller/MainController.java
+++ week-07/day-03/hellodi/src/main/java/com/greenfoxacademy/hellodi/Controller/MainController.java
@@ -4,6 +4,7 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestParam;
 
 @Controller
 public class MainController {
@@ -17,8 +18,31 @@ public class MainController {
   }
 
   @GetMapping("/useful/colored")
-  public String showUsefulPage(Model model) {
+  public String showColoredBackgroundPage(Model model) {
     model.addAttribute("utilityservice", utilityService.randomColor());
     return "colored";
   }
+
+  @GetMapping("/useful/email")
+  public String showEmailValidationPage(@RequestParam ("email") String email, Model model) {
+    model.addAttribute("validator", utilityService.validateEmail(email));
+    model.addAttribute("email", email);
+    return "email";
+  }
+
+  @GetMapping("/useful/encode")
+  public String encodingPage(@RequestParam ("text") String text, @RequestParam ("number") int number, Model model) {
+    model.addAttribute("codedtext", utilityService.caesar(text, number));
+    return "coding";
+  }
+
+  @GetMapping("/useful/decode")
+  public String decodingPage(@RequestParam ("text") String text, @RequestParam ("number") int number, Model model) {
+    model.addAttribute("codedtext", utilityService.caesar(text, -number));
+    return "coding";
+  }
+
+
+
+
 }
--- week-07/day-03/hellodi/src/main/java/com/greenfoxacademy/hellodi/Service/UtilityService.java
+++ week-07/day-03/hellodi/src/main/java/com/greenfoxacademy/hellodi/Service/UtilityService.java
@@ -1,6 +1,7 @@
 package com.greenfoxacademy.hellodi.Service;
 import org.springframework.stereotype.Service;
 import java.util.ArrayList;
+import java.util.Arrays;
 import java.util.Random;
 
 @Service
@@ -20,5 +21,20 @@ public class UtilityService {
 
   public String randomColor() {
     return colors.get(random.nextInt(colors.size()));
+  }
+
+  public boolean validateEmail(String email) {
+    if (email.contains("@") && email.contains(".")) {
+      return true;
+    }
+    return false;
+  }
+
+  public String caesar(String text, int number) {
+    String result = "";
+    for(int i = 0; i < text.length(); i++) {
+      result += (char)((int)text.charAt(i) + number);
+    }
+    return result;
   }
 }
