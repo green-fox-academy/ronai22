78da28491154e7a10b7088e4501f06e693269b57
ronai22 <ronai22@gmail.com>
2018-07-03 13:36:36 +0200

    init(listing-todo): initialized project

--- week-08/day-02/listing-todos/src/main/java/com/greenfoxacademy/listingtodos/ListingTodosApplication.java
+++ week-08/day-02/listing-todos/src/main/java/com/greenfoxacademy/listingtodos/ListingTodosApplication.java
@@ -6,7 +6,7 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 @SpringBootApplication
 public class ListingTodosApplication {
 
-	public static void main(String[] args) {
-		SpringApplication.run(ListingTodosApplication.class, args);
-	}
+  public static void main(String[] args) {
+    SpringApplication.run(ListingTodosApplication.class, args);
+  }
 }
--- week-08/day-02/listing-todos/src/test/java/com/greenfoxacademy/listingtodos/ListingTodosApplicationTests.java
+++ week-08/day-02/listing-todos/src/test/java/com/greenfoxacademy/listingtodos/ListingTodosApplicationTests.java
@@ -9,8 +9,8 @@ import org.springframework.test.context.junit4.SpringRunner;
 @SpringBootTest
 public class ListingTodosApplicationTests {
 
-	@Test
-	public void contextLoads() {
-	}
+  @Test
+  public void contextLoads() {
+  }
 
 }
