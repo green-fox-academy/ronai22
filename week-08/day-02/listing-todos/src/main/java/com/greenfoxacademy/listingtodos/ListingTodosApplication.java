fe89f421d4cb9520f738fb6d8281701d3576c4bf
ronai22 <ronai22@gmail.com>
2018-07-03 13:29:45 +0200

    modifications to make screenshots for demo

---
+++ week-08/day-02/listing-todos/src/main/java/com/greenfoxacademy/listingtodos/ListingTodosApplication.java
@@ -0,0 +1,12 @@
+package com.greenfoxacademy.listingtodos;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ListingTodosApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ListingTodosApplication.class, args);
+	}
+}
