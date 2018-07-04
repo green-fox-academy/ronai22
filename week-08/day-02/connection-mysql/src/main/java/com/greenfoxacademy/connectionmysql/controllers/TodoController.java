82a000961e874706c4092ce6bc6055a895a89bb2
ronai22 <ronai22@gmail.com>
2018-07-04 11:56:48 +0200

    feat(connection with mySQL): initialize connection

---
+++ week-08/day-02/connection-mysql/src/main/java/com/greenfoxacademy/connectionmysql/controllers/TodoController.java
@@ -0,0 +1,30 @@
+package com.greenfoxacademy.connectionmysql.controllers;
+import com.greenfoxacademy.connectionmysql.repositories.TodoRepository;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
+
+@Controller
+@RequestMapping(value="/todo")
+public class TodoController {
+
+  TodoRepository todoRepository;
+
+  @Autowired
+  public TodoController(TodoRepository todoRepository) {
+    this.todoRepository = todoRepository;
+  }
+
+  @RequestMapping(value="", method=RequestMethod.GET)
+  public String list2(Model model) {
+    return "redirect:/todo/list";
+  }
+
+  @RequestMapping(value="/list", method=RequestMethod.GET)
+  public String list(Model model) {
+    model.addAttribute("todos", todoRepository.findAll());
+    return "todolist";
+  }
+}
---
+++ week-08/day-02/connection-mysql/src/main/java/com/greenfoxacademy/connectionmysql/repositories/TodoRepository.java
@@ -0,0 +1,10 @@
+package com.greenfoxacademy.connectionmysql.repositories;
+
+import com.greenfoxacademy.connectionmysql.models.Todo;
+import org.springframework.data.repository.CrudRepository;
+import org.springframework.stereotype.Repository;
+
+@Repository
+public interface TodoRepository extends CrudRepository<Todo, Long> {
+
+}
