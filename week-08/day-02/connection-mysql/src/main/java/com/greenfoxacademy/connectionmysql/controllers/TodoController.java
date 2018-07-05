41e0cdc1a22530b7efbdb89c0788e0cd14326958
ronai22 <ronai22@gmail.com>
2018-07-05 15:07:23 +0200

    feat(todo online): implement bootstrap design

--- week-08/day-02/connection-mysql/src/main/java/com/greenfoxacademy/connectionmysql/controllers/TodoController.java
+++ week-08/day-02/connection-mysql/src/main/java/com/greenfoxacademy/connectionmysql/controllers/TodoController.java
@@ -31,6 +31,7 @@ public class TodoController {
 
   @RequestMapping(value="/list", method=RequestMethod.GET)
   public String list(Model model) {
+    model.addAttribute("newTodo", new Todo());
     model.addAttribute("todos", todoRepository.findAll());
     return "todolist";
   }
