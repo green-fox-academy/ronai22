58a265135848ac21e3a64d8d854b99c9c0d62532
ronai22 <ronai22@gmail.com>
2018-07-06 09:53:00 +0200

    init(reddit-app): initialize gradle for project

---
+++ week-08/day-05/new-reddit/src/main/java/com/greenfoxacademy/newreddit/NewRedditApplication.java
@@ -0,0 +1,12 @@
+package com.greenfoxacademy.newreddit;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class NewRedditApplication {
+
+  public static void main(String[] args) {
+    SpringApplication.run(NewRedditApplication.class, args);
+  }
+}
---
+++ week-08/day-05/new-reddit/src/test/java/com/greenfoxacademy/newreddit/NewRedditApplicationTests.java
@@ -0,0 +1,16 @@
+package com.greenfoxacademy.newreddit;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
+
+@RunWith(SpringRunner.class)
+@SpringBootTest
+public class NewRedditApplicationTests {
+
+  @Test
+  public void contextLoads() {
+  }
+
+}
