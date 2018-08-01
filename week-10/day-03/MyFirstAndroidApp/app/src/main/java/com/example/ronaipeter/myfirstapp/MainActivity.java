2097e0f0a83a3931a34ec7c2a8d386d0365f9ce1
ronai22 <ronai22@gmail.com>
2018-08-01 14:24:43 +0200

    reformat code

--- week-10/day-03/MyFirstAndroidApp/app/src/main/java/com/example/ronaipeter/myfirstapp/MainActivity.java
+++ week-10/day-03/MyFirstAndroidApp/app/src/main/java/com/example/ronaipeter/myfirstapp/MainActivity.java
@@ -11,17 +11,19 @@ public class MainActivity extends AppCompatActivity {
   public static final String EXTRA_MESSAGE = "com.example.myfirstapp.MESSAGE";
 
   @Override
-    protected void onCreate(Bundle savedInstanceState) {
-        super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_main);
-    }
+  protected void onCreate(Bundle savedInstanceState) {
+    super.onCreate(savedInstanceState);
+    setContentView(R.layout.activity_main);
+  }
 
-    /** Called when the user taps the Send button */
-    public void sendMessage(View view) {
-        Intent intent = new Intent(this, DisplayMessageActivity.class);
-        EditText editText = (EditText) findViewById(R.id.editText);
-        String message = editText.getText().toString();
-        intent.putExtra(EXTRA_MESSAGE, message);
-        startActivity(intent);
-    }
+  /**
+   * Called when the user taps the Send button
+   */
+  public void sendMessage(View view) {
+    Intent intent = new Intent(this, DisplayMessageActivity.class);
+    EditText editText = (EditText) findViewById(R.id.editText);
+    String message = editText.getText().toString();
+    intent.putExtra(EXTRA_MESSAGE, message);
+    startActivity(intent);
+  }
 }
